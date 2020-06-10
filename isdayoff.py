192ddac55e9229d8c4615df9c453d8c8c02df5b8
KMVSoft <mkv-1724@mail.ru>
2020-06-10 04:10:47 +0300

    Версия 0.1

---
+++ isdayoff.py
@@ -0,0 +1,25 @@
+from enum import IntEnum
+from datetime import date
+from functools import lru_cache
+from typing import Optional
+import requests
+
+URL = 'https://isdayoff.ru/'
+DATE_FORMAT = '%Y%m%d'
+
+
+class DayType(IntEnum):
+    working = 0
+    not_working = 1
+
+
+@lru_cache
+def check(day: date) -> Optional[DayType]:
+    req = requests.get(URL+day.strftime(DATE_FORMAT))
+    if req.status_code != 200:
+        return None
+    return DayType(int(req.text))
+
+
+def today() -> Optional[DayType]:
+    return check(date.today())
