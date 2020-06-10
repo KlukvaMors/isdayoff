f5b579a985f0a72bc3a21cd756d63a48ff7f1db3
KMVSoft <mkv-1724@mail.ru>
2020-06-10 05:32:58 +0300

    кеширование идёт на диск

--- isdayoff.py
+++ isdayoff.py
@@ -1,11 +1,16 @@
+"""Производственный календарь.  Обёртка на API https://isdayoff.ru/. Есть кеширование результатов"""
 from enum import IntEnum
-from datetime import date
+from datetime import date, timedelta
 from functools import lru_cache
 from typing import Optional
 import requests
+from joblib import Memory
 
 URL = 'https://isdayoff.ru/'
 DATE_FORMAT = '%Y%m%d'
+CACHE_DIR = "cache"
+
+memory = Memory(CACHE_DIR)
 
 
 class DayType(IntEnum):
@@ -13,7 +18,7 @@ class DayType(IntEnum):
     NOT_WORKING = 1
 
 
-@lru_cache
+@memory.cache
 def check(day: date) -> Optional[DayType]:
     req = requests.get(URL+day.strftime(DATE_FORMAT))
     if req.status_code != 200:
