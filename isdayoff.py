346d06c4bd99a247f4fb98586d2172c3f7640cc0
KMVSoft <mkv-1724@mail.ru>
2020-06-12 00:29:51 +0300

    переделано в класс

--- isdayoff.py
+++ isdayoff.py
@@ -1,16 +1,14 @@
-"""Производственный календарь.  Обёртка на API https://isdayoff.ru/. Есть кеширование результатов"""
 from enum import IntEnum
 from datetime import date, timedelta
-from functools import lru_cache
 from typing import Optional
+import calendar
+
 import requests
 from joblib import Memory
 
-URL = 'https://isdayoff.ru/'
-DATE_FORMAT = '%Y%m%d'
-CACHE_DIR = "cache"
 
-memory = Memory(CACHE_DIR)
+class ServiceNotRespond(Exception):
+    pass
 
 
 class DayType(IntEnum):
@@ -18,13 +16,38 @@ class DayType(IntEnum):
     NOT_WORKING = 1
 
 
-@memory.cache
-def check(day: date) -> Optional[DayType]:
-    req = requests.get(URL+day.strftime(DATE_FORMAT))
-    if req.status_code != 200:
-        return None
-    return DayType(int(req.text))
+class ProdCalendar:
+    """Производственный календарь.  Обёртка на API https://isdayoff.ru/. Есть кеширование результатов"""
+    URL = 'https://isdayoff.ru/'
+    DATE_FORMAT = '%Y%m%d'
+    CACHE_DIR = 'cache'
 
+    def __init__(self, cache=True):
+        self.memory = Memory(self.CACHE_DIR if cache else None, verbose=1)
 
-def today() -> Optional[DayType]:
-    return check(date.today())
+    def _check(self, day: date) -> DayType:
+        req = requests.get(self.URL + day.strftime(self.DATE_FORMAT))
+        if req.status_code != 200:
+            raise ServiceNotRespond
+        return DayType(int(req.text))
+
+    def check(self, day: date) -> DayType:
+        return self.memory.cache(self._check)(day)
+
+    def today(self) -> DayType:
+        return self.check(date.today())
+
+    def next_work_day(self, day: date) -> date:
+        while self.check(day) != DayType.WORKING:
+            day += timedelta(days=1)
+        return day
+
+    def previous_work_day(self, day: date) -> date:
+        while self.check(day) != DayType.WORKING:
+            day -= timedelta(days=1)
+        return day
+
+    def cache_month(self, year: int, month: int):
+        """Кеширование результатов на месяц"""
+        for day in range(1, calendar.monthrange(year, month)[1]+1):
+            self.check(date(year, month, day))
