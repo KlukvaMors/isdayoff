0bd9c2da47e75db444685baab310951df6d30986
KMVSoft <mkv-1724@mail.ru>
2020-06-18 19:43:12 +0300

    Обновлено кеширование
    Теперь в ProdCalendar появилось понятие свежесть кеша (freshness) -- это период времени, за который кеш полностью обновляется. Также изменены некоторые моменты кеширования.

--- isdayoff.py
+++ isdayoff.py
@@ -1,10 +1,9 @@
 from enum import IntEnum
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 from pathlib import Path
 import calendar
 
 import requests
-
 
 
 class ServiceNotRespond(Exception):
@@ -23,19 +22,20 @@ class ProdCalendar:
     CACHE_FILE_FORMAT = '%sisdayoff%i%s.txt'
     LOCALES = ('ru', 'ua', 'kz', 'by')
 
-    def __init__(self, locale: str = 'ru', cache=True, cache_dir: str = 'cache/', cache_year: int = date.today().year):
+    def __init__(self, locale: str = 'ru', cache: bool = True, cache_dir: str = 'cache/',
+                 cache_year: int = date.today().year, freshness: timedelta = timedelta(days=30)):
         if locale not in self.LOCALES:
             raise ValueError('locale must be in '+str(self.LOCALES))
         self.locale = locale
         self.cache = cache
         self.cache_dir = cache_dir
+        self.freshness = freshness
         if cache:
             self.cache_year(cache_year, forced=False)
 
     def check(self, day: date) -> DayType:
         if self.cache:
-            with open(self.cache_year(day.year, forced=False)) as f:
-                return DayType(int(f.read(day.timetuple().tm_yday)[-1]))
+            return self._get_cache(day)
         else:
             req = requests.get(self.URL + day.strftime(self.DATE_FORMAT), params={'cc': self.locale})
             if req.status_code != 200:
@@ -60,12 +60,28 @@ class ProdCalendar:
         Path(self.cache_dir).mkdir(exist_ok=True, parents=True)
         cache_file = Path(self.CACHE_FILE_FORMAT % (self.cache_dir, year, self.locale))
         if forced or not cache_file.is_file():
-            res = requests.get(self.URL + 'api/getdata', params={'year': year, 'cc': self.locale})
-            if not res.status_code == 200:
-                raise ServiceNotRespond()
-            with open(cache_file.absolute(), 'w') as f:
-                f.write(res.text)
+            self._write_cache(year, cache_file)
+        elif not self._is_cache_fresh(cache_file):
+            self._write_cache(year, cache_file)
 
         return cache_file.absolute()
+
+    def _write_cache(self, year, cache_file):
+        res = requests.get(self.URL + 'api/getdata', params={'year': year, 'cc': self.locale})
+        if not res.status_code == 200:
+            raise ServiceNotRespond()
+        with open(cache_file.absolute(), 'w') as f:
+            f.write(datetime.now().isoformat()+'\n')
+            f.write(res.text)
+
+    def _is_cache_fresh(self, cache_file) -> bool:
+        with open(cache_file.absolute()) as f:
+            return datetime.fromisoformat(f.readline().strip()) + self.freshness >= datetime.now()
+
+    def _get_cache(self, day: date) -> DayType:
+        with open(self.cache_year(day.year, forced=False)) as f:
+            f.readline()  # pass cache date
+            return DayType(int(f.readline(day.timetuple().tm_yday)[-1]))
+
 
 
